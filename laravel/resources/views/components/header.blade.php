f6860d597614b8dfa7d4e6300cf7b443bf08c2ea
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-22 18:29:53 +0200

    setting up components

---
+++ laravel/resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>e-Store</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"> </script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.8.0/dist/chart.min.js"></script>
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+        
---
+++ laravel/resources/views/user/login.blade.php
@@ -0,0 +1,29 @@
+<x-header></x-header>
+    <div class="w-full flex flex-col h-screen items-center justify-center">
+        <div action="signup.php" method="POST" 
+            class="flex items-center shadow-xl border-[1px] flex-col p-4 w-[400px] min-w-[280px] ">
+            <h2 class="text-2xl font-semibold text-blue-500 text-center">Log into e-Store</h2>
+
+            <div class="flex flex-wrap text-red-400"><?php  ?></div>
+            <div class="flex flex-col w-full mt-4">
+                <label for="name" class="font-semibold">Email</label>
+                <input type="email" placeholder="enter your email" name="email" id="name" class="w-full border-[2px]
+                  rounded-lg h-[40px] mt-2 px-3 text-lg outline-none border-slate-800 focus:border-blue-400">
+            </div>
+            <div class="flex flex-col w-full mt-4">
+                <label for="password" class="font-semibold">Password</label>
+                <input type="password" placeholder="enter your password" name="password" id="password" class="w-full border-[2px]
+                  rounded-lg h-[40px] mt-2 px-3 text-lg outline-none border-slate-800 focus:border-blue-400">
+            </div>
+            
+            <input name="submit" class="bg-blue-500 px-4 py-2 mx-auto w-full 
+             cursor-pointer text-white rounded-lg mt-4" type="submit" value="Log In">
+
+             <div class="flex w-full mt-4 items-center">
+                <p class="mb-0">Don't have an account?</p>
+                <a class="ml-3 text-blue-600" href="/users/signup">Sign Up</a>
+             </div>
+        </div>
+    </div>
+</body>
+</html>
